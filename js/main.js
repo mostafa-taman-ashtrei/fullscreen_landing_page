3a79e47dba748e803ffa84d9a71a73b70dd1fd76
NEO <neo.one.mail@gmail.com>
2021-06-15 09:23:09 +0200

    set up fullpage scrolling

---
+++ js/main.js
@@ -0,0 +1,10 @@
+new fullpage('#fullPage', {
+    autoScrolling: true,
+    navigation: true,
+    anchors: ['section1', 'section2', 'section3'],
+    navigationTooltips: ['Home', 'About', 'Contnat'],
+    showActiveTooltip: true,
+    scrollingSpeed: 1000,
+    controlArrows:false,
+    slidesNavigation: true,
+});
